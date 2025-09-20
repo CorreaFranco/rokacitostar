b7c722d7b4533f0ae4fcc53107044a4d1028f211
correafranco <francoantonycs@gmail.com>
2025-09-20 12:04:27 -0500

    eliminacion accesorios y adicion de calzdo

--- scripts/productos.js
+++ scripts/productos.js
@@ -12,9 +12,9 @@ class Producto {
 let prendas = [];
 let accesorios = [];
 let nombresPrendas = ["prenda", "prenda", "prenda", "prenda", "prenda", "prenda","prenda","prenda","prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda", "prenda"];
-let nombresAccesorios = ["Accesorio", "Accesorio", "Accesorio", "Accesorio", "Accesorio", "Accesorio", "Accesorio", "Accesorio", "Accesorio", "Accesorio"];
+let nombresAccesorios = ["Calzado", "Calzado"];
 let preciosPrendas = [5, 6, 7, 8, 10, 12, 13, 15, 16, 18, 20, 22, 25, 26, 28, 30, 35, 38, 40, 45, 48, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 150];
-let preciosAccesorios = [5, 10, 15, 20, 25, 30, 35, 40, 45, 65];
+let preciosAccesorios = [30, 35,];
 
 nombresPrendas.forEach((nombre, index) => {
     let id = (index + 1).toString(); // Los IDs empiezan en "1"
